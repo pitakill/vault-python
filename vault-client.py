a4ffac02974679a15cba9b7aeb469effbde1945b
Leopoldo Caballero <git@pitakill.net>
2020-02-13 13:18:50 -0600

    Query Vault from Python
    1. Why is this change necessary?
    Basic change to handle the information petition
    
    2. How does it address the issue?
    Creates a basic health status of the Vault from Python
    
    3. What side effects does this change have?
    None

---
+++ vault-client.py
@@ -0,0 +1,13 @@
+import hvac
+import os
+
+client = hvac.Client(
+    url=os.environ['VAULT_ADDR'],
+    token=os.environ['VAULT_TOKEN'],
+)
+
+if not client.is_authenticated():
+    print("Can't communicate with Vault")
+
+status = client.sys.read_health_status(method='GET')
+print(status)
